1197a98691716252bf2e17c1ca009fb9e5524811
Ironbuff <hinsane82@gmail.com>
2025-01-26 22:17:18 +0545

    react-commit

--- Design/Form/src/App.jsx
+++ Design/Form/src/App.jsx
@@ -1,16 +1,17 @@
 import Form1 from "./components/Form/Form1";
+import Form2 from "./components/Form/Form2";
 import { BrowserRouter as Router } from "react-router-dom";
 import React from "react";
-import Form2 from "./components/Foem/Form2";
+
 function App() {
   return (
     <>
       <Router>
         <div className="w-full flex items-center justify-center py-10 relative">
           {/* Background Image */}
-          <div className="w-1/2 aspect-video bg-[url('./assests/map.png') bg-cover bg-center rounded-xl">
+          <div className="w-1/2 aspect-video bg-[url('./assests/map.png')] bg-cover bg-center rounded-xl">
             {/* content */}
-            <div className="w-full h-full relative z-20 p-6 ">
+            <div className="w-full h-full relative z-20 p-6">
               <Form1 />
               <Form2 />
             </div>
@@ -26,10 +27,8 @@ function App() {
             <div className="w-full h-full relative z-20 p-6 bg-indigo-500">
               sdlkfdklshkl
               <h1 className="text-red-500 text-xl">Hello there</h1>
-
             </div>
-            </div> */}
-
+          </div> */}
         {/* Overlay */}
         {/* <div className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] w-1/2 aspect-video bg-black opacity-60 rounded-xl z-10"></div>
           </div>
