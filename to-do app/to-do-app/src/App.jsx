ca897452342c528757d227265f695948ef9132b1
Ironbuff <hinsane82@gmail.com>
2025-01-27 20:22:58 +0545

    to-do-app

--- to-do app/to-do-app/src/App.jsx
+++ to-do app/to-do-app/src/App.jsx
@@ -1,32 +1,48 @@
-import { useState } from 'react'
-import React from 'react'
-import Input from './components/Input'
-import Todo from './components/Todo'
+import React, { useState } from 'react';
+import Input from './components/Input';
+import Todo from './components/Todo';
 
 const App = () => {
-  const[list,SetList]=useState([]);//we initalize with blank array since it will store list value
-  //we define fn to store data from child component to parent component we use prop
-  let addlist = (input)=>{
-    if(input!=='')
-      SetList([...list,input]);
-  }
-  const dellist=(key)=>{
-    let updatedlist = [...list];
-    updatedlist.splice(key,1)
-    SetList([...updatedlist])
-  }
-  return (
-    <div className='flex flex-col'>
-      <Input addlist={addlist}/>
-     <h1 className='text-2xl font-bold text-blue-600'>TODO</h1>
-     <hr/>
-     {list.map((list,i)=>{ //two argument list array and item
-      return(
-        <Todo key={i} index={i} item={list} deleteitem={dellist}/> //here item we pass array of task to perform
-      )
-     })}
-    </div>
-  )
-}
+  const [list, setList] = useState([]); // Initialize state for the list
 
-export default App
+  // Function to add an item to the list
+  const addToList = (input) => {
+    if (input.trim()) {
+      setList((prevList) => [...prevList, input.trim()]);
+    }
+  };
+
+  // Function to delete an item from the list
+  const deleteFromList = (index) => {
+    setList((prevList) => prevList.filter((_, i) => i !== index));
+  };
+
+  return (
+    <div className="flex flex-col items-center p-4">
+      {/* Input Component */}
+      <Input onAdd={addToList} />
+
+      {/* Header */}
+      <h1 className="text-3xl font-bold text-blue-600 my-4">TODO List</h1>
+      <hr className="w-full border-gray-300 mb-4" />
+
+      {/* Todo Items */}
+      {list.length > 0 ? (
+        <div className="w-full space-y-2">
+          {list.map((item, i) => (
+            <Todo 
+              key={item + i} // Use a more unique key
+              index={i} 
+              task={item} 
+              onDelete={deleteFromList} 
+            />
+          ))}
+        </div>
+      ) : (
+        <p className="text-gray-500">Your todo list is empty. Add a task above!</p>
+      )}
+    </div>
+  );
+};
+
+export default App;
