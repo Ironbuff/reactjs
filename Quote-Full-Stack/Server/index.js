407a4c5c30cf6172667ae7ddf4ac0c8e83ca959b
Ironbuff <hinsane82@gmail.com>
2025-02-07 19:00:54 +0545

    server user commit

--- Quote-Full-Stack/Server/index.js
+++ Quote-Full-Stack/Server/index.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import cors from 'cors'
 import authRouter from './routes/auth.js'
+import connectToMongoDB from './db/db.js'
 // Assigning variable a express fn 
 const app= express()
 
 // Enabling CORS (Cross-Origin Resource Sharing) for the application
 // CORS allows your backend to accept requests from different domains (e.g., frontend running on a different URL)
 app.use(cors())
+app.use(express.json())
 
 // Setting up a middleware for authentication-related routes
 // All routes inside 'authRouter' will be prefixed with '/api/auth'
@@ -15,6 +17,7 @@ app.use('/api/auth',authRouter)
 
 // here 5000 is the port no and another is the function
 app.listen(5000, ()=>{
+  connectToMongoDB()
   console.log("The Server is running.")
 }) 
 // It is used to show that the server is running in the background.
--- Quote-Full-Stack/Server/routes/auth.js
+++ Quote-Full-Stack/Server/routes/auth.js
@@ -1,6 +1,7 @@
 // Importing express, a web framework for Node.js used to create server applications
 import express from 'express'; 
-
+import User from '../models/User'
+import bcrypt from 'bcrypt'
 // Creating an instance of the Express Router
 // The router helps in organizing routes (API endpoints) separately
 const router = express.Router();
@@ -9,6 +10,23 @@ const router = express.Router();
 // When a request is sent to '/register', this function will handle it
 router.post('./register', async (res, req) => {
     // TODO: Implement the logic for handling user registration
+    try{
+               const {name, email, password}=req.body;
+               const user= await UserActivation.findOne({email})
+               if(user){
+                return res.status(401).json({success: false, message:"User already exist"})
+               }
+               const hashPassword = await bcrypt.hash(password, 10)
+
+               const newUser = new User({
+                name, email, passowrd:hashPassword
+               })
+               await newUser.save()
+               return res.status(200).json({success:true, message:"Accounted Created Sucessfully"})
+
+    }catch(error){
+        return res.status(200).json({success:false, message:"Error in Adding User"})
+    }
 });
 
 // Exporting the router so it can be used in other parts of the application
