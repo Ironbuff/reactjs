93eb5baf3c76f63cb8582bcc303826fe5c35de2c
Ironbuff <hinsane82@gmail.com>
2025-02-11 21:06:14 +0545

    Additional Page

--- Quote-Full-Stack/Quote-App/src/App.jsx
+++ Quote-Full-Stack/Quote-App/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Signup from './pages/Signup'
-import { BrowserRouter as Router } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 function App() {
   return (
     <> 
     <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/register' element={<Signup/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+      </Routes>
      <Signup/>
 
     </Router>
