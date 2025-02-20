ec80034cc8b7a781dca8701acf63f6918e2594fe
Ironbuff <hinsane82@gmail.com>
2025-02-20 20:58:08 +0545

    simple commit

--- Ecommerce-App/Ecommerce-site/src/components/Navbar.jsx
+++ Ecommerce-App/Ecommerce-site/src/components/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { CiSearch } from "react-icons/ci";
+import image from "../assets/Ecommerce-logo.jpg"
 const Navbar = () => {
   return (
     <div>
         {/* Logo Section */}
         <div>
-            <img src='../assests/Ecommerce-logo.jpg'></img>
+            <img src={image} className='aspect-video h-24 w-24'></img>
         </div>
         {/* Search Button */}
-        <div>
+        <div className='border border-black'>
            <input
            type='text'
            placeholder='search '
