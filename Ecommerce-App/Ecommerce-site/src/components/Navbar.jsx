99cf911ae837d302b276d276fdb913ca625e349b
Ironbuff <hinsane82@gmail.com>
2025-02-21 20:11:16 +0545

    first navbar commit

--- Ecommerce-App/Ecommerce-site/src/components/Navbar.jsx
+++ Ecommerce-App/Ecommerce-site/src/components/Navbar.jsx
@@ -3,16 +3,17 @@ import { CiSearch } from "react-icons/ci";
 import image from "../assets/Ecommerce-logo.jpg"
 const Navbar = () => {
   return (
-    <div>
+    <div className='flex flex-row px-28 items-center justify-between'>
         {/* Logo Section */}
         <div>
             <img src={image} className='aspect-video h-24 w-24'></img>
         </div>
         {/* Search Button */}
-        <div className='border border-black'>
+        <div className='border border-black w-[25%] flex flex-row justify-between px-7 items-center rounded-4xl '>
            <input
            type='text'
-           placeholder='search '
+           placeholder='Search'
+           className='outline-none '
            />
            <CiSearch />
         </div>
