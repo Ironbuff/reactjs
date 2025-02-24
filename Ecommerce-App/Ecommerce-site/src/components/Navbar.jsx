516b5abc18df0ed1ed3f6a1054fa9c3a1bf3ab71
Ironbuff <hinsane82@gmail.com>
2025-02-24 21:26:34 +0545

    git commit

--- Ecommerce-App/Ecommerce-site/src/components/Navbar.jsx
+++ Ecommerce-App/Ecommerce-site/src/components/Navbar.jsx
@@ -3,10 +3,11 @@ import { IoIosSearch } from "react-icons/io";
 import image from "../assets/Ecommerce-logo.jpg"
 const Navbar = () => {
   return (
-    <div className='flex flex-row px-28 items-center justify-between'>
+    <div className='flex flex-row px-28 items-center justify-between bg-white shadow-sm'>
         {/* Logo Section */}
-        <div>
+        <div className='flex flex-row items-center'>
             <img src={image} className='aspect-video h-24 w-24'></img>
+            <p className='text-4xl font-bold text-red-600'>SAJ<span className='text-yellow-500'>ILO</span></p>
         </div>
         {/* Search Button */}
         <div className='border border-black w-[20%] justify-between flex flex-row  px-2 items-center rounded-4xl '>
