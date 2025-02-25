1d506bee5240e3d22a1ea0d5406726107657ae24
Ironbuff <hinsane82@gmail.com>
2025-02-25 21:19:20 +0545

    card - page start

---
+++ Ecommerce-App/Ecommerce-site/src/components/Card.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+        <div>
+           <div>
+            Products Image
+            </div>
+            <h1>
+                Price
+            </h1>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Card
