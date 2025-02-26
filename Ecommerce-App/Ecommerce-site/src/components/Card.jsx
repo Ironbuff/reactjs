4e2423a4a169d68bb57e0f464a7804b99e27dc26
Ironbuff <hinsane82@gmail.com>
2025-02-26 21:01:48 +0545

    array start

--- Ecommerce-App/Ecommerce-site/src/components/Card.jsx
+++ Ecommerce-App/Ecommerce-site/src/components/Card.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
 const Card = () => {
+  const Products =[
+    {id:1,name:"watch",link:"https://cdn.pixabay.com/photo/2014/07/31/23/00/wristwatch-407096_1280.jpg"},
+    {id:2,name:"glass", link:"https://cdn.pixabay.com/photo/2015/07/28/17/10/safety-glasses-864648_1280.jpg"},
+    {id:3, name:"hat", link:"https://cdn.pixabay.com/photo/2016/12/04/23/22/hat-1882816_1280.jpg"},
+  ]
   return (
     <div>
         <div>
