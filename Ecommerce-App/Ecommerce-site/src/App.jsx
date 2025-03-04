e9e6732f80a920fbb8bafdcfc9d19273d2f21509
Ironbuff <hinsane82@gmail.com>
2025-03-04 20:49:34 +0545

    first Commit

--- Ecommerce-App/Ecommerce-site/src/App.jsx
+++ Ecommerce-App/Ecommerce-site/src/App.jsx
@@ -1,17 +1,19 @@
-import Card from './components/Card'
-import Customer from './components/Customer'
-import Navbar from './components/Navbar'
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Card from './components/Card';
+import Customer from './components/Customer';
+import Navbar from './components/Navbar';
 
 function App() {
- 
   return (
-    <>
-     <Navbar/>
-     <Card/>
-     <Customer/>
-    </>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/card" element={<Card />} />
+        <Route path="/customer" element={<Customer />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
