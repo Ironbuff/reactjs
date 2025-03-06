898a3e767840c273c7e4549c0ac1702f5277a1b6
Ironbuff <hinsane82@gmail.com>
2025-03-06 18:26:29 +0545

    New Footer

--- Ecommerce-App/Ecommerce-site/src/components/Footer.jsx
+++ Ecommerce-App/Ecommerce-site/src/components/Footer.jsx
@@ -1,25 +1,50 @@
-import React from 'react'
-import image from "../assets/Logo.jpg"
-import { Link } from "react-router-dom";
-const Footer = () => {
-  
-    return (
-    <div className='flex items-center justify-between px-28 bg-neutral-800 h-[12ch]'>
-        <div className='flex items-center justify-between'>
-            <img src={image} className='w-32 h-32 object-fit aspect-video'/>
-        </div>
-        <div>
-            <ul className='list-none flex flex-row gap-5 '>
-                <li className='text-xl font-bold border  px-4 py-1  hover:bg-gray-300 text-neutral-500'>
-                    <Link to="/">Products</Link>
-                </li>
-                <li className='text-xl font-bold border  px-4 py-1  hover:bg-gray-300 text-neutral-500'>
-                    <Link to="/">Customer Reviews</Link>
-                </li>
-            </ul>
-        </div>
-    </div>
-  )
-}
+import React from 'react';
+import image from '../assets/Logo.jpg';
+import { Link } from 'react-router-dom';
+import { FaFacebook, FaInstagram } from 'react-icons/fa';
+import { RiTwitterXLine } from 'react-icons/ri';
 
-export default Footer
+const Footer = () => {
+  const socialLinks = [
+    { icon: <FaFacebook />, id: 1 },
+    { icon: <FaInstagram />, id: 2 },
+    { icon: <RiTwitterXLine />, id: 3 },
+  ];
+
+  return (
+    <div className='flex flex-col md:flex-row items-center justify-between px-10 md:px-28 py-6 bg-neutral-800 text-neutral-300'>
+      {/* Logo and Socials */}
+      <div className='flex flex-col items-center md:items-start gap-4'>
+        <img src={image} className='w-24 h-24 object-cover' alt='Logo' />
+        <div className='text-center md:text-left'>
+          <h1 className='text-lg font-semibold'>Connect with Us</h1>
+          <div className='flex gap-4 mt-2'>
+            {socialLinks.map((item) => (
+              <div
+                key={item.id}
+                className='text-xl p-3 bg-neutral-700 hover:bg-neutral-600 rounded-full transition duration-300 cursor-pointer'
+              >
+                {item.icon}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      
+      {/* Services */}
+      <div className='mt-6 md:mt-0 text-center md:text-left'>
+        <h1 className='text-lg font-semibold border-b-2 border-neutral-500 pb-2'>Our Services</h1>
+        <ul className='mt-3 space-y-2'>
+          <li>
+            <Link to='/card' className='hover:text-white transition duration-300'>Products</Link>
+          </li>
+          <li>
+            <Link to='/customer' className='hover:text-white transition duration-300'>Customer Reviews</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- Ecommerce-App/Ecommerce-site/src/components/Navbar.jsx
+++ Ecommerce-App/Ecommerce-site/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { IoIosSearch } from "react-icons/io";
 import image from "../assets/Logo.jpg"
 const Navbar = () => {
   return (
-    <div className='flex flex-row px-28 items-center justify-between bg-white shadow-sm'>
+    <div className='flex flex-row px-28 h-[12ch] items-center justify-between bg-white shadow-sm'>
         {/* Logo Section */}
         <div className='flex flex-row items-center'>
             <img src={image} className='aspect-video h-32 w-32'></img>
