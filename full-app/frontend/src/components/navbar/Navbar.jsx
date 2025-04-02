8f80c0c1de3efc9b7943ab8edc92976d77492ae5
Ironbuff <hinsane82@gmail.com>
2025-04-02 21:12:05 +0545

    new-design

--- full-app/frontend/src/components/navbar/Navbar.jsx
+++ full-app/frontend/src/components/navbar/Navbar.jsx
@@ -1,70 +1,96 @@
-import React from 'react'
-import { FaBook } from "react-icons/fa";
-import userimage from '../../assests/user.png'
+import React from 'react';
+import { FaBook, FaUser, FaTasks, FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
+import userImage from '../../assests/user.png';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
 import { authActions } from '../../store';
 
-const Nabar = () => {
-    // Check if user is logged in by looking at the current app state
-    const isLoggedIn = useSelector((state)=> state.isLoggedIn);
-    const dispatch = useDispatch();
+const Navbar = () => {
+  // Check if user is logged in by looking at the current app state
+  const isLoggedIn = useSelector((state) => state.isLoggedIn);
+  const dispatch = useDispatch();
+  
+  const logout = () => {
+    sessionStorage.clear("id");
+    dispatch(authActions.loggout()); //convert value of is loggin from true to false
+  };
 
-
-    const logout = () =>{
-        sessionStorage.clear("id")
-       dispatch(authActions.loggout()); //convert value of is loggin from true to false
-    }
-
-    // Print the login status to see if user is logged in or not
-    console.log(isLoggedIn)
+  // Print the login status to see if user is logged in or not
+  console.log(isLoggedIn);
 
   return (
-    <div className='flex flex-row justify-between px-28 items-center h-[12ch] border-b border-neutral-100 shadow-md'>
-        <div className='flex flex-row gap-x-5 items-center'>
-            <FaBook className='object-fit h-15 text-red-500'/>
-            <h1 className='font-bold text-2xl text-red-500'>
-                <Link to="/"> todo</Link>
-                </h1>
-        </div>
-        <div className='flex items-center'>
-            <ul className='list-none flex flex-row gap-x-3'>
-                <li className='px-2 py-2 bg-red-500 text-neutral-100 rounded-lg'>
-                   <Link to='/todo'> Todo </Link>
-                </li>
+    <nav className="bg-white shadow-md">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-16">
+          {/* Logo and Brand Name */}
+          <div className="flex items-center">
+            <Link to="/" className="flex items-center space-x-3 hover:opacity-90 transition duration-150">
+              <FaBook className="h-8 w-8 text-red-600" />
+              <span className="font-bold text-2xl text-red-600 tracking-tight">todo</span>
+            </Link>
+          </div>
+          
+          {/* Navigation Links */}
+          <div className="flex items-center">
+            <div className="hidden md:block">
+              <div className="flex items-center space-x-4">
+                <Link 
+                  to="/todo" 
+                  className="flex items-center space-x-1 px-4 py-2 bg-red-600 text-white rounded-md font-medium hover:bg-red-700 transition duration-150"
+                >
+                  <FaTasks className="h-4 w-4" />
+                  <span>Todo</span>
+                </Link>
                 
                 {/* here if user is logged in it shows login and sign in option */}
                 {!isLoggedIn && (
-                    <>
-                    <li className='px-2 py-2 bg-red-500 text-neutral-100 rounded-lg'>
-                   <Link to='/login'> LogIn </Link>
-                </li>
-                <li className='px-2 py-2 bg-red-500 text-neutral-100 rounded-lg'>
-                <Link to="/signin"> SignIn</Link>
-                </li>
-                </>
+                  <>
+                    <Link 
+                      to="/login" 
+                      className="flex items-center space-x-1 px-4 py-2 bg-gray-100 text-gray-800 rounded-md font-medium hover:bg-gray-200 transition duration-150"
+                    >
+                      <FaSignInAlt className="h-4 w-4" />
+                      <span>LogIn</span>
+                    </Link>
+                    <Link 
+                      to="/signin" 
+                      className="flex items-center space-x-1 px-4 py-2 border border-red-600 text-red-600 rounded-md font-medium hover:bg-red-50 transition duration-150"
+                    >
+                      <FaUser className="h-4 w-4" />
+                      <span>SignIn</span>
+                    </Link>
+                  </>
                 )}
-         
+                
                 {/* if user is logged In then they are shown this option */}
-                {isLoggedIn &&(
-                  <> 
-                  <li className='px-2 py-2 bg-red-500 text-neutral-100 rounded-lg' onClick={logout}>
-                   <Link to='/'> LogOut </Link>
-                </li>
-                
-                    
-                <li>
-                <img src={userimage} className='aspect-ratio flex items-center w-auto h-10'/>
-               </li>
-               </>
+                {isLoggedIn && (
+                  <div className="flex items-center space-x-4">
+                    <Link 
+                      to="/" 
+                      onClick={logout}
+                      className="flex items-center space-x-1 px-4 py-2 bg-gray-100 text-gray-800 rounded-md font-medium hover:bg-gray-200 transition duration-150"
+                    >
+                      <FaSignOutAlt className="h-4 w-4" />
+                      <span>LogOut</span>
+                    </Link>
+                    <div className="flex items-center">
+                      <div className="h-10 w-10 rounded-full overflow-hidden border-2 border-gray-200">
+                        <img 
+                          src={userImage} 
+                          alt="User Profile" 
+                          className="h-full w-full object-cover"
+                        />
+                      </div>
+                    </div>
+                  </div>
                 )}
-
-
-                
-            </ul>
+              </div>
+            </div>
+          </div>
         </div>
-    </div>
-  )
-}
+      </div>
+    </nav>
+  );
+};
 
-export default Nabar
+export default Navbar;
