bc12ff179aae58b30ea6cd34c308c8b0179d74da
Ironbuff <hinsane82@gmail.com>
2025-04-05 22:16:06 +0545

    change

--- full-app/frontend/src/components/pages/signin/SignIn.jsx
+++ full-app/frontend/src/components/pages/signin/SignIn.jsx
@@ -48,12 +48,12 @@ const SignIn = () => {
                 <form className='flex flex-col gap-y-4' onSubmit={submit}> {/* Apply onSubmit to the form */}
                     {/* Email field */}
                     <div className='flex flex-col'>
-                        <label htmlFor='email' className='text-red-500 font-semibold mb-1'>Email:</label>
+                        <label htmlFor='email' className='text-neutral-500 font-semibold mb-1'>Email:</label>
                         <input
                             type='email'
                             id='email'
                             name='email' // Added name attribute for controlled components
-                            className='w-full px-4 py-2 rounded-lg border-1 outline-none text-red-500 placeholder-neutral-800'
+                            className='w-full px-4 py-2 rounded-lg border-1 outline-none text-neutral-500 placeholder-neutral-800'
                             placeholder='Enter Email'
                             onChange={change}
                             value={inputs.email}
@@ -62,7 +62,7 @@ const SignIn = () => {
 
                     {/* Username field */}
                     <div className='flex flex-col'>
-                        <label htmlFor='username' className='text-red-500 font-semibold mb-1'>Username:</label>
+                        <label htmlFor='username' className='text-neutral-500 font-semibold mb-1'>Username:</label>
                         <input
                             type='text'
                             id='username'
@@ -76,7 +76,7 @@ const SignIn = () => {
 
                     {/* Password field */}
                     <div className='flex flex-col'>
-                        <label htmlFor='password' className='text-red-500 font-semibold mb-1'>Password:</label>
+                        <label htmlFor='password' className='text-neutral-500 font-semibold mb-1'>Password:</label>
                         <input
                             type='password'
                             id='password'
