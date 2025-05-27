098bd99f7adb0d2ddc4e8373b748cfa2dc909b01
Ironbuff <hinsane82@gmail.com>
2025-05-27 13:25:03 +0545

    Frontend commit

--- Cloth-Store/Frontend/src/App.jsx
+++ Cloth-Store/Frontend/src/App.jsx
@@ -1,19 +1,24 @@
 import React from "react"
-import { BrowserRouter as Router,Routes,Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Home from "./pages/home/Home"
 import Clothdetail from "./components/clothdetail/Clothdetail"
 import Login from "./pages/login/Login"
+import {Provider} from 'react-redux'
+import Store from "./store"
 function App() {
 
   return (
+
     <Router>
-      <Navbar/>
-       <Routes>
-         <Route path="/" element={<Home/>}/>
-         <Route path="/detail/:id" element={<Clothdetail/>}/>
-         <Route path='/login' element={<Login/>}/>
-       </Routes>
+      <Provider store={Store}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/detail/:id" element={<Clothdetail />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </Provider>
     </Router>
   )
 }
