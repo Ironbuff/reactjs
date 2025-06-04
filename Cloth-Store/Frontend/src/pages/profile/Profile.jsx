bd414f4168e153dcec236f6d1522eec5502144b1
Ironbuff <hinsane82@gmail.com>
2025-06-04 22:47:35 +0545

    sidebar and Outlet Commit

--- Cloth-Store/Frontend/src/pages/profile/Profile.jsx
+++ Cloth-Store/Frontend/src/pages/profile/Profile.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <div className='h-screen w-full flex flex-row items-center justify-center'>
+      
+      <div className='w-2/5 h-full'>
+      <Sidebar />
+      </div>
+      <div>
+      <Outlet className='w-3/5 h-full'/>
+      </div>
+    </div>
   )
 }
 
