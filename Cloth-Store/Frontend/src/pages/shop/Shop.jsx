b92318d76b34b8d6c2e099fe63068c20629b9b12
Ironbuff <hinsane82@gmail.com>
2025-06-30 23:19:28 +0545

    Winter Filter Clothes Commit

--- Cloth-Store/Frontend/src/pages/shop/Shop.jsx
+++ Cloth-Store/Frontend/src/pages/shop/Shop.jsx
@@ -1,28 +1,50 @@
-import React from 'react'
-import { useEffect } from 'react'
-import axios from 'axios'
-import { useState } from 'react'
-import Clothcard from '../../components/clothcard/Clothcard'
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Clothcard from '../../components/clothcard/Clothcard';
 
 const Shop = () => {
-  
-  const[datas,setDatas]=useState([])
-  
-    useEffect(()=>{
+  const [datas, setDatas] = useState([]);
+  const [selectedData, setSelectedData] = useState(null);
+  const [isWinterChecked, setIsWinterChecked] = useState(false);
 
-     const fetch = async()=>{
-        const response = await axios.get('http://localhost:8081/api/user/clothes/getcloth')
-        setDatas(response.data)
-     }
-     fetch()
-  },[])
-  
-    return (
-    <div>
-     <Clothcard item={datas}/>
+  useEffect(() => {
+    const fetch = async () => {
+      const response = await axios.get('http://localhost:8081/api/user/clothes/getcloth');
+      setDatas(response.data);
+    };
+    fetch();
+  }, []);
+
+  const handleFilter = (e) => {
+    const checked = e.target.checked;
+    setIsWinterChecked(checked);
+
+    if (checked) {
+      const winterClothes = datas.filter(item => item.category === "winter clothes");
+      setSelectedData(winterClothes);
+    } else {
+      setSelectedData(null);
+    }
+  };
+
+  return (
+    <div className='px-28 bg-gray-200'>
+      <div className='flex  gap-x-3'>
+        <label htmlFor='winterClothes'>
+          Winter clothes
+        </label>
+        <input
+          id='winterClothes'
+          type='checkbox'
+          className='rounded-xl bg-transparent'
+          checked={isWinterChecked}
+          onChange={handleFilter}
+        />
+      </div>
+
+      <Clothcard item={selectedData ? selectedData : datas} />
     </div>
-  )
-}
+  );
+};
 
-export default Shop
+export default Shop;
