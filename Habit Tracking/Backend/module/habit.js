c5aac81094ba70d654f6af8e86223adda0887d33
Ironbuff <hinsane82@gmail.com>
2025-07-20 22:28:51 +0545

    Added Habit Schema

---
+++ Habit Tracking/Backend/module/habit.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const { type } = require('os')
+const Schema = mongoose.Schema
+
+
+const HabitSchema = new Schema({
+    title:{
+        type:String,
+        required:true,
+        minlength:3,
+    },
+    description:{
+        type:String,
+        minlength:3
+    },
+    completedDates:[{
+         type:Date
+    }],
+    user:{
+        type:Schema.Types.ObjectId,
+        ref:'User'
+    }
+},{timestamps:true})
+
+
+module.exports = mongoose.model("Habit",HabitSchema)
+
