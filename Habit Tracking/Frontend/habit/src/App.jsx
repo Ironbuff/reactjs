f90731808eedefb3f92fe1c247ae5df8d2efef43
Ironbuff <hinsane82@gmail.com>
2025-08-13 23:40:22 +0545

    All Commit

---
+++ Habit Tracking/Frontend/habit/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import { BrowserRouter as Router } from "react-router-dom"
+import Navbar from "./components/navbar/Navbar"
+
+function App() {
+
+
+  return (
+    <>
+      <Router>
+        <Navbar/>
+      </Router>
+    
+    </>
+  )
+}
+
+export default App
---
+++ Habit Tracking/Frontend/habit/src/components/navbar/Navbar.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  
+    const item = [
+        {id:1, title:"Login", links:"/login"},
+        {id:2, title:"Register", links:"/register"},
+        {id:3, title:"Add Habit", links:"/habit"},
+        {id:4, title:"Logout", links:"/logout"}
+    ]
+  
+  
+    return (
+    <div className='flex flex-row items-center w-full h-[9ch] justify-between px-28'>
+        <h1 className='font-bold text-2xl flex items-center justify-center text-red-300'>
+            Habit
+            <span className='text-neutral-700 px-1'>
+                App
+            </span>
+        </h1>
+        <div className='flex flex-row items-center justify-center gap-x-2'>
+            {item.map((value)=>(
+                <Link to={value.links} key={value.id} className='text-base font-normal p-2 rounded-xl bg-red-400 hover:bg-red-500 text-neutral-100 hover:text-neutral-50 duration-300 ease-in-out transition-all '>
+                    {value.title}
+                </Link>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
