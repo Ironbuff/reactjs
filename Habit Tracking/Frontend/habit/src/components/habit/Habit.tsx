1a927bfe0cfa8b6ddaad3e360aea68732ced2e35
Ironbuff <hinsane82@gmail.com>
2025-08-16 22:38:59 +0545

    New Comapny

--- Habit Tracking/Frontend/habit/src/components/habit/Habit.tsx
+++ Habit Tracking/Frontend/habit/src/components/habit/Habit.tsx
@@ -1,15 +1,31 @@
 import React from 'react'
-import UseGetHabits from '../../libary/GetHabitsValue'
+import { getHabits } from '../../services/GetHabits'
+import { useQuery } from '@tanstack/react-query'
 
 const Habit = () => {
-  
-   const {data} = UseGetHabits()
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['habit'],
+    queryFn: getHabits,
+  })
 
-   console.log({data})
-  
-    return (
+  console.log(data?.data)
+
+  if (isLoading) {
+    return <p>Loading...</p>
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>
+  }
+
+  return (
     <div>
-
+      <h2>Habits</h2>
+      <ul>
+        {data?.data?.newhabit?.map((habit) => (
+          <li key={habit._id}>{habit.title}</li>
+        ))}
+      </ul>
     </div>
   )
 }
