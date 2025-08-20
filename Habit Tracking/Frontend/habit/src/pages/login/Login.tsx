6a3e2568cd393587ed60db7f57131f38dfbce7c1
Ironbuff <hinsane82@gmail.com>
2025-08-20 22:41:29 +0545

    Added Error Message in login

--- Habit Tracking/Frontend/habit/src/pages/login/Login.tsx
+++ Habit Tracking/Frontend/habit/src/pages/login/Login.tsx
@@ -1,13 +1,30 @@
 import React from "react";
+import { FormSchema } from "../../schema/LoginSchema";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(FormSchema),
+  });
+
+  const onSubmit = (data) => {
+    console.log("Login data:", data);
+  };
+
   return (
     <div className="flex items-center justify-center min-h-screen ">
       <div className="w-full max-w-md p-8 space-y-6 bg-gray-50 shadow-lg rounded-2xl">
         <h2 className="text-3xl font-bold text-center text-gray-800">
           Login
         </h2>
-        <form className="space-y-5">
+
+        <form className="space-y-5" onSubmit={handleSubmit(onSubmit)}>
           {/* Email */}
           <div>
             <label
@@ -20,8 +37,12 @@ const Login = () => {
               type="email"
               id="email"
               placeholder="Enter your email"
+              {...register("email")}
               className="w-full px-4 py-2 text-gray-900 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-indigo-500"
             />
+            {errors.email && (
+              <p className="mt-1 text-sm text-red-500">{errors.email.message}</p>
+            )}
           </div>
 
           {/* Password */}
@@ -36,14 +57,18 @@ const Login = () => {
               type="password"
               id="password"
               placeholder="Enter your password"
+              {...register("password")}
               className="w-full px-4 py-2 text-gray-900 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-indigo-500"
             />
+            {errors.password && (
+              <p className="mt-1 text-sm text-red-500">{errors.password.message}</p>
+            )}
           </div>
 
           {/* Submit */}
           <button
             type="submit"
-            className="w-full py-2 text-white  bg-red-200 hover:bg-red-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-indigo-500 transition duration-300"
+            className="w-full py-2 text-white bg-red-200 hover:bg-red-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-indigo-500 transition duration-300"
           >
             Login
           </button>
---
+++ Habit Tracking/Frontend/habit/src/schema/LoginSchema.tsx
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const FormSchema = z.object({
+    username: z.string().min(1,"UserName is Required"),
+    password: z.string()
+        .min(6,"Password Length must be atleast 6 letter long")
+        .regex(/[a-z]/,"It must contain atleast one small character")
+        .regex(/[A-Z]/,"It must contain atleast one capital letter")
+        .regex(/[0-9]/,"Password must contain atleast one number")
+        .regex(/[^A-Za-z0-9]/, "Password must have at least one special character"),
+    email: z.email().min(1,"Email is Required")
+})
