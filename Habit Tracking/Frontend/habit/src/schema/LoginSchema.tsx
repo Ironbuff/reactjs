ba2fffaa45dc4347db5b14b965e78614f1825dc0
Ironbuff <hinsane82@gmail.com>
2025-10-17 21:43:52 +0545

    schema rule addition

--- Habit Tracking/Frontend/habit/src/schema/LoginSchema.tsx
+++ Habit Tracking/Frontend/habit/src/schema/LoginSchema.tsx
@@ -22,3 +22,22 @@ export const LoginSchema = z.object({
     .regex(/[0-9]/, "Password must contain atleast one number")
     .regex(/[^A-Za-z0-9]/, "Password must have at least one special character"),
 });
+
+export const ResetPasswordSchema = z
+  .object({
+    password: z
+      .string()
+      .min(6, "Password Must be atleast 6 letter long")
+      .regex(/[a-z]/, "It must contain atleast one small character")
+      .regex(/[A-Z]/, "It must contain atleast one capital letter")
+      .regex(/[0-9]/, "Password must contain atleast one number")
+      .regex(
+        /[^A-Za-z0-9]/,
+        "Password must have at least one special character"
+      ),
+    confirmPassword: z.string().min(1, "Please confirm your password."),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match.",
+    path: ["confirmPassword"], // Apply error to the confirmPassword field
+  });
