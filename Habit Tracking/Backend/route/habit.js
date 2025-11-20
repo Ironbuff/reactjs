496a7f334e4f55ff6c5bcd07a5917c8601abf22d
Ironbuff <hinsane82@gmail.com>
2025-11-20 22:43:47 +0545

    left the issue

--- Habit Tracking/Backend/route/habit.js
+++ Habit Tracking/Backend/route/habit.js
@@ -3,10 +3,11 @@ const router = express.Router()
 const habitController = require('../controller/habit-controller')
 const authVerify = require('../middleware/verify')
 const optionalAuth = require('../middleware/optionalauth')
+const upload = multer({ dest: 'uploads/' })
 
 
 router.get('/',optionalAuth,habitController.getUserHabit)
-router.post('/addhabit',authVerify,habitController.addHabit)
+router.post('/addhabit',authVerify,upload.single('image'),habitController.addHabit)
 router.delete('/:ids',authVerify,habitController.deleteUserHabit)
 router.post('/toggle/:id',authVerify,habitController.toggleHabitCompletion)
 router.put('/updatehabit/:id',authVerify,habitController.updateHabit)
