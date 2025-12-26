690383734d996d0ffb606323b5ef22032640a699
Ironbuff <hinsane82@gmail.com>
2025-12-26 23:57:44 +0545

    fixed routing issue in the code

--- BookStore-app/frontend/src/components/home/Hero.jsx
+++ BookStore-app/frontend/src/components/home/Hero.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
-import image from '../../assests/New Libary.png';
+import React from "react";
+import image from "../../assests/New Libary.png";
+import { useNavigate } from "react-router-dom";
 
 const Hero = () => {
+  const navigate = useNavigate();
   return (
-    <section className="flex flex-col-reverse md:flex-row items-center justify-between w-full min-h-[calc(100vh-13ch)] bg-neutral-900 px-6 py-12">
-      
+    <section className="flex px-12 flex-col-reverse md:flex-row items-center justify-between w-full min-h-[calc(100vh-13ch)] bg-neutral-900  py-12">
       {/* Text Content */}
-      <div className="w-full md:w-1/2 lg:w-2/5 text-center md:text-left text-neutral-100 space-y-6">
-        <h1 className="text-4xl sm:text-5xl font-bold leading-tight">
+      <div className="w-full md:w-1/2 ml-12 lg:w-2/5 text-center md:text-left text-neutral-100 space-y-6">
+        <h1 className="text-4xl sm:text-5xl font-bold  leading-tight">
           Discover Your Next <br className="hidden sm:inline" /> Great Read
         </h1>
         <p className="text-base sm:text-lg max-w-md mx-auto md:mx-0 text-neutral-300">
-          Uncover captivating stories, enriching knowledge, and endless inspiration in our curated collection of books.
+          Uncover captivating stories, enriching knowledge, and endless
+          inspiration in our curated collection of books.
         </p>
         <div className="pt-2">
-          <button className="inline-block text-lg sm:text-xl font-semibold border border-neutral-200 px-6 py-3 rounded-xl bg-neutral-800 hover:bg-blue-500 hover:text-white transition-all duration-300">
+          <button
+            onClick={() => {
+              navigate("/book");
+            }}
+            className="inline-block text-lg sm:text-xl font-semibold border border-neutral-200 px-6 py-3 rounded-xl bg-neutral-800 hover:bg-blue-500 hover:text-white transition-all duration-300"
+          >
             Discover Books
           </button>
         </div>
@@ -22,9 +29,9 @@ const Hero = () => {
 
       {/* Image Section */}
       <div className="w-full md:w-1/2 lg:w-2/5 mb-10 md:mb-0 flex justify-center">
-        <img 
-          src={image} 
-          alt="Library Books" 
+        <img
+          src={image}
+          alt="Library Books"
           className="object-contain max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg h-auto rounded-xl shadow-lg"
         />
       </div>
