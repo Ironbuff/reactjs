5b568bafd68dc342b692fd01e169bda3a16f18eb
Ironbuff <hinsane82@gmail.com>
2026-01-21 22:41:00 +0545

    added Route for food controller

--- FoodStore/Backend/route/food-route.js
+++ FoodStore/Backend/route/food-route.js
@@ -1,12 +1,28 @@
-const FoodController = require('../controller/food-controller')
-const express = require('express')
-const router = express.Router()
-const {optionalAuth} = require('../middleware/optionalauth')
-const multer = require('multer')
+const FoodController = require("../controller/food-controller");
+const express = require("express");
+const router = express.Router();
+const { optionalAuth } = require("../middleware/optionalauth");
+const multer = require("multer");
 
-const upload = multer({dest:'uploads/'})
+const upload = multer({ dest: "uploads/" });
 
-router.post('/add',optionalAuth,upload.single('image'),FoodController.addFoodCollection)
-router.get('/',FoodController.getFoodCollection)
+router.post(
+  "/add",
+  optionalAuth,
+  upload.single("image"),
+  FoodController.addFoodCollection,
+);
+router.get("/", FoodController.getFoodCollection);
+(router.put(
+  "/update/:id",
+  optionalAuth,
+  upload.single("image"),
+  FoodController.editFoodCollection,
+),
+  router.delete(
+    "/delete/:id",
+    optionalAuth,
+    FoodController.deleteFoodCollection,
+  ));
 
 module.exports = router;
