e7477ade906a06fd323d90d218a67a5a6922b130
Ironbuff <hinsane82@gmail.com>
2026-01-26 22:32:59 +0545

    fixed route for user

--- FoodStore/Backend/route/user-route.js
+++ FoodStore/Backend/route/user-route.js
@@ -1,10 +1,10 @@
-const userController = require('../controller/user-controller')
-const express = require('express');
-const router = express.Router()
+const userController = require("../controller/user-controller");
+const express = require("express");
+const router = express.Router();
+const optionalAuth = require("../middleware/optionalauth");
 
+router.post("/sign", userController.sign);
+router.post("/login", userController.login);
+router.post("/role", optionalAuth, userController.setRole);
 
-router.post('/sign',userController.sign)
-router.post('/login',userController.login)
-
-
-module.exports = router
+module.exports = router;
