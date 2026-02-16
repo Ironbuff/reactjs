d7794455f7808cc32fbcd2860b82343547d079c1
Ironbuff <hinsane82@gmail.com>
2026-02-16 22:53:13 +0545

    started design of home page

--- FoodStore/Frontend/food-store-app/app/page.tsx
+++ FoodStore/Frontend/food-store-app/app/page.tsx
@@ -1,8 +1,12 @@
+import HomeScreen from "@/components/home/HomeScreen";
 import { Navbar } from "@/components/navbar/navbar";
 
 
 export default function Home() {
   return (
+    <>
         <Navbar/>
+        <HomeScreen/>
+        </>
   );
 }
---
+++ FoodStore/Frontend/food-store-app/components/home/HomeScreen.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center px-7 h-full py-10">
+      <h1 className="text-2xl font-semibold text-center px-2 py-2 rounded-md border-neutral-700 font-mono bg-neutral-700 text-neutral-200">
+        Order Food <br/>We Are Here to Serve You.
+      </h1>
+
+      <div className="relative w-[500px] h-[500px]">
+        <Image
+          src="/cup Cake.jpg"
+          alt="Kitchen"
+          fill
+          className="object-cover rounded-md"
+          priority
+        />
+      </div>
+    </div>
+  );
+}
