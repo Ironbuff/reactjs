5c64770f585020ecbf7432bd6ce4d08fe4752181
Ironbuff <hinsane82@gmail.com>
2026-02-24 22:51:31 +0545

    Added add food page to main code

---
+++ FoodStore/Frontend/food-store-app/app/add/page.tsx
@@ -0,0 +1,14 @@
+import AddScreen from '@/components/add/add'
+import { Navbar } from '@/components/navbar/navbar'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <Navbar/>
+    <AddScreen/>
+   </>
+  )
+}
+
+export default page
