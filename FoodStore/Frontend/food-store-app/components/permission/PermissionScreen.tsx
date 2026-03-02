d5ca836a71efb7cd2a81d889d7efc454c8f2bfa6
Ironbuff <hinsane82@gmail.com>
2026-03-02 20:44:15 +0545

    adjusted the width of input field

--- FoodStore/Frontend/food-store-app/components/permission/PermissionScreen.tsx
+++ FoodStore/Frontend/food-store-app/components/permission/PermissionScreen.tsx
@@ -11,11 +11,11 @@ import { userList } from "@/constants/FoodStoreConstant";
 
 const PermissionScreen = () => {
   return (
-    <div className="flex flex-col gap-y-4">
-      <h1>Select Permission</h1>
-      <Combobox items={userList}>
-        <ComboboxInput placeholder="Select a framework" />
-        <ComboboxContent>
+    <div className="flex flex-col gap-y-4 px-7 py-4">
+      <h1 className="text-xl font-semibold ">Select Permission For Different Users:</h1>
+      <Combobox items={userList} >
+        <ComboboxInput placeholder="Select a framework" className={'w-[800px]'} />
+        <ComboboxContent className={'hidden'}>
           <ComboboxEmpty>No items found.</ComboboxEmpty>
           {userList.map((item) => (
             <ComboboxItem key={item.value} value={item.value}>
