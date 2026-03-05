df40dbfb33f8dfe2d7b24f1447f5319f2ecf6a54
Ironbuff <hinsane82@gmail.com>
2026-03-05 20:43:02 +0545

    added permission page

--- FoodStore/Frontend/food-store-app/components/permission/PermissionScreen.tsx
+++ FoodStore/Frontend/food-store-app/components/permission/PermissionScreen.tsx
@@ -8,22 +8,63 @@ import {
   ComboboxList,
 } from "../ui/combobox";
 import { userList } from "@/constants/FoodStoreConstant";
+import { Controller, useForm } from "react-hook-form";
+import {z} from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const permissionSchema = z.object({
+role:z.string().optional(),
+})
+
+type permissionStoreType =  z.infer<typeof permissionSchema>
+
+const permissionform = useForm({
+  resolver:zodResolver(permissionSchema),
+  defaultValues:{
+    role:''
+  }
+})
+
+
+  const onSubmit = (data:permissionStoreType) => {
+    console.log("Submitted Data:", data);
+  };
 
 const PermissionScreen = () => {
   return (
     <div className="flex flex-col gap-y-4 px-7 py-4">
       <h1 className="text-xl font-semibold ">Select Permission For Different Users:</h1>
-      <Combobox items={userList} >
-        <ComboboxInput placeholder="Select a framework" className={'w-[800px]'} />
-        <ComboboxContent >
-          <ComboboxEmpty>No items found.</ComboboxEmpty>
-          {userList.map((item) => (
-            <ComboboxItem key={item.value} value={item.id}>
-              {item.id}
-            </ComboboxItem>
-          ))}
-        </ComboboxContent>
-      </Combobox>
+      <form onSubmit={permissionform.handleSubmit(onSubmit)}>
+        <Controller
+          control={permissionform.control}
+          name="role"
+          render={({ field }) => (
+            <Combobox items={userList} value={field.value} onValueChange={field.onChange}>
+              <ComboboxInput
+                placeholder="Select a user"
+                className="w-[400px]"
+              />
+
+              <ComboboxContent>
+                <ComboboxEmpty>No items found.</ComboboxEmpty>
+
+                {userList.map((item) => (
+                  <ComboboxItem key={item.value} value={item.value}>
+                    {item.id}
+                  </ComboboxItem>
+                ))}
+              </ComboboxContent>
+            </Combobox>
+          )}
+        />
+
+        <button
+          type="submit"
+          className="mt-4 bg-black text-white px-4 py-2 rounded"
+        >
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
