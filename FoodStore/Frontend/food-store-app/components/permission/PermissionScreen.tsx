60c12b2707c49e7dc54c16f5063b3b9b0933fa69
Ironbuff <hinsane82@gmail.com>
2026-03-08 22:01:40 +0545

    added shadcn form in the form

--- FoodStore/Frontend/food-store-app/components/permission/PermissionScreen.tsx
+++ FoodStore/Frontend/food-store-app/components/permission/PermissionScreen.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import React from "react";
 import {
@@ -8,10 +8,19 @@ import {
   ComboboxInput,
   ComboboxItem,
 } from "../ui/combobox";
+
 import { userList } from "@/constants/FoodStoreConstant";
-import { Controller, useForm } from "react-hook-form";
+
+import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+} from "@/components/ui/form";
 
 const permissionSchema = z.object({
   role: z.string().optional(),
@@ -20,8 +29,7 @@ const permissionSchema = z.object({
 type PermissionStoreType = z.infer<typeof permissionSchema>;
 
 const PermissionScreen = () => {
-
-  const permissionform = useForm<PermissionStoreType>({
+  const form = useForm<PermissionStoreType>({
     resolver: zodResolver(permissionSchema),
     defaultValues: {
       role: "",
@@ -33,46 +41,55 @@ const PermissionScreen = () => {
   };
 
   return (
-    <div className="flex flex-col gap-y-4 px-7 py-4 h-full  w-full items-center justify-center">
+    <div className="flex flex-col gap-y-4 px-7 py-4 h-full w-full items-center justify-center">
       <h1 className="text-xl font-semibold">
         Select Permission For Different Users:
       </h1>
 
-      <form onSubmit={permissionform.handleSubmit(onSubmit)} className="border border-transparent shadow-md px-2 py-2 rounded-md">
-        <Controller
-          control={permissionform.control}
-          name="role"
-          render={({ field }) => (
-            <Combobox
-              items={userList}
-              value={field.value}
-              onValueChange={field.onChange}
-            >
-              <ComboboxInput
-                placeholder="Select a user"
-                className="w-[400px]"
-              />
-
-              <ComboboxContent>
-                <ComboboxEmpty>No items found.</ComboboxEmpty>
-
-                {userList.map((item) => (
-                  <ComboboxItem key={item.value} value={item.value}>
-                    {item.id}
-                  </ComboboxItem>
-                ))}
-              </ComboboxContent>
-            </Combobox>
-          )}
-        />
-
-        <button
-          type="submit"
-          className="mt-4 bg-black text-white px-4 py-2 rounded"
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="border border-transparent shadow-md px-2 py-2 rounded-md"
         >
-          Submit
-        </button>
-      </form>
+          <FormField
+            control={form.control}
+            name="role"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Combobox
+                    items={userList}
+                    value={field.value}
+                    onValueChange={field.onChange}
+                  >
+                    <ComboboxInput
+                      placeholder="Select a user"
+                      className="w-[400px]"
+                    />
+
+                    <ComboboxContent>
+                      <ComboboxEmpty>No items found.</ComboboxEmpty>
+
+                      {userList.map((item) => (
+                        <ComboboxItem key={item.value} value={item.value}>
+                          {item.id}
+                        </ComboboxItem>
+                      ))}
+                    </ComboboxContent>
+                  </Combobox>
+                </FormControl>
+              </FormItem>
+            )}
+          />
+
+          <button
+            type="submit"
+            className="mt-4 bg-black text-white px-4 py-2 rounded"
+          >
+            Submit
+          </button>
+        </form>
+      </Form>
     </div>
   );
 };
