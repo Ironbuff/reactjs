69ab016f909d2a4676a412233ab79a40e14b5f23
Ironbuff <hinsane82@gmail.com>
2026-06-23 22:55:11 +0545

    added default image for food module

--- FoodStore/Backend/models/food-modal.js
+++ FoodStore/Backend/models/food-modal.js
@@ -1,33 +1,33 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const foodSchema = new Schema({
-    title:{
-        type:String,
-        required:true,
-        minlength:2,
-    },
-    description:{
-        type:String,
-        required:true,
-        minlength:2
-    },
-    image:{
-        type:String,
-    },
-    price:{
-        type:Number,
-        required:true,
-    },
-    discount:{
-        type:Number,
-        required:false,
-    },
-    user:{
-        type:Schema.Types.ObjectId,
-        ref:'User'
-    }
-})
+  title: {
+    type: String,
+    required: true,
+    minlength: 2,
+  },
+  description: {
+    type: String,
+    required: true,
+    minlength: 2,
+  },
+  image: {
+    type: String,
+    default: "public/dummy.jpg",
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  discount: {
+    type: Number,
+    required: false,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
 
-
-module.exports = mongoose.model('Food',foodSchema)
+module.exports = mongoose.model("Food", foodSchema);
